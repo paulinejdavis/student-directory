235a79a0483bc284403733a87e80ec25dae17277
ppjd <pauljdavi.t21@btinternet.com>
2022-09-09 09:57:37 +0000

    added each do function

--- directory.rb
+++ directory.rb
@@ -1,18 +1,21 @@
-student_count = 11
-
-#list of students
+students = [
+    "Dr. Hannibal Lecter", 
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Coreleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+]
+# print array
 puts "The students of Villians Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Coreleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#number of students
-print "Overall, we have #{student_count} great students\n"
+puts "--------------"
+students.each do |student|
+    puts student
+end
+#end with printing the total
+puts "Overall, we have #{students.count} great students"
