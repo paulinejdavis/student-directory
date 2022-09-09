65ad686f36043f146436290f1ada24cf3f487263
ppjd <pauljdavi.t21@btinternet.com>
2022-09-09 13:40:04 +0000

    cleaned up code and added comments

--- directory.rb
+++ directory.rb
@@ -1,32 +1,3 @@
-students = [
-    {name:"Dr. Hannibal Lecter", cohort: :november},
-    {name:"Darth Vader", cohort: :november},
-    {name:"Nurse Ratched", cohort: :november},
-    {name:"Michael Coreleone", cohort: :november},
-    {name:"Alex DeLarge", cohort: :november},
-    {name:"The Wicked Witch of the West", cohort: :november},
-    {name:"Terminator", cohort: :november},
-    {name:"Freddy Krueger", cohort: :november},
-    {name:"The Joker", cohort: :november},
-    {name:"Joffrey Baratheon", cohort: :november},
-    {name:"Norman Bates", cohort: :november}
-]
-
-def print_header
-    puts "The students of Villians Academy"
-    puts "--------------"
-end
-
-def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-end
-
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
-end
-
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
@@ -46,11 +17,24 @@ def input_students
     students
 end
 
+def print_header
+    puts "The students of Villians Academy"
+    puts "--------------"
+end
+
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+
+
 students = input_students
+#nothing happens until methods are called
 print_header
 print(students)
 print_footer(students)
-
-# print_header
-# puts (students)
-# print_footer(students)
