55f9b41708a8c527cb659206420c6fb0727f93bf
ppjd <pauljdavi.t21@btinternet.com>
2022-09-11 08:01:25 +0000

    interactive functions

--- directory.rb
+++ directory.rb
@@ -1,47 +1,44 @@
 @students = []
 
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
-    puts "Please enter the names of the students"
+   puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    
-    name = STDIN.gets.chomp
+    #create an empty array
+    students = []
+    #get first name
+    name = gets.chomp
+    #while the name is not empty, repeat code
     while !name.empty? do
-        puts "Please enter the cohort"
-        cohort = STDIN.gets.chomp
-        if cohort == ""
-            cohort = "Unassigned"
-        else
-            cohort = cohort.to_sym
-        end
-        puts "Enter country"
-        country = STDIN.gets.chomp
-        puts "Enter age"
-        age = STDIN.gets.chomp
-        puts "Enter height"
-        height = STDIN.gets.chomp
-        puts "Enter hobbies"
-        hobbies = STDIN.gets.chomp
-        @students << {name: name, cohort: cohort, country: country, age: age, height: height, hobbies: hobbies}
-        puts "Now we have #{@students.count} students"   
-        puts "\n Enter a new name. "
-        puts"(To finish, just hit return)"
-        name = STDIN.gets.chomp
-        
-        
-        
-        
-        
-        # if cohort.empty? then cohort = "december" end
-        # Date: : ABBR_MONTHNAMES.include? cohort ? cohort.to_sym : cohort = :default_cohort
-        #   puts "Now we have #{students.count} students#{@students.count > 1 ? 's' : ''}."   
-        # #add student hash to the array
-        # students << {name: name, cohort: cohort, country: country, height: height, hobbies: hobbies}
-        # puts "\n Enter a new name. "
-        # puts"(To finish, just hit return)"
-        # # name = STDIN.gets.strip
-      
+        #add student hash to the array
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
         #get another name from user
-        # name = gets. chomp
+        name = gets. chomp
     end
     #return array of students
     students
@@ -60,17 +57,6 @@ def print(students)
     end
 end
 
-# def student_questions(student)
-#          puts "Enter country of".center(@width)
-#         country = STDIN.gets.chomp
-#         student[:country] = country
-#         puts "Enter hobbies of".center(@width)
-#         hobbies = STDIN.gets.chomp
-#         student[:hobbies] = hobbies
-#         puts "Enter height of".center(@width)
-#         height = STDIN.gets.chomp
-#         student[:height] = height
-# end
     
 def printonlyD(students)
     puts "The students whose name begins with D are:"
@@ -87,15 +73,16 @@ def long_name(students)
         if student[:name].length > 12
             puts "#{student[:name]} (#{student[:cohort]} cohort)"
         end
-    end
 end
-
+end
 
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+    if names.count == 1
+     puts "Overall, we have 1 great student"
+    else
+   puts "Overall, we have #{names.count} great students"
+    end
 end
-
-
 students = input_students
 #nothing happens until methods are called
 print_header
