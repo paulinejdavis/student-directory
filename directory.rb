07b3b9ea7fea44bb072024fb4ba285494bbb97c0
ppjd <pauljdavi.t21@btinternet.com>
2022-09-11 08:18:10 +0000

    refactorisation

--- directory.rb
+++ directory.rb
@@ -1,28 +1,38 @@
 @students = []
 
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
+end
+    
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to file"
+  puts "4. Load the list from file"
+  puts "5. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
